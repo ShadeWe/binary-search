1b5ddfe1984c3836924ee766623c05061633bba5
ShadeWe <slim.shady.ghetto@yandex.ru>
2019-08-01 23:02:31 +0400

    large files support (seeking on a file pointer)

--- index.php
+++ index.php
@@ -1,47 +1,71 @@
 <?php
 
-$start = microtime(true);
+/*
+ *   Get the number of lines in the provided file
+ */
+function getNumberOfValues($filename) {
 
-function binarySearchByKey($filename, $key_value) {
+	$file = fopen($filename, 'r');
+	$count = 0;
 
-	$data = file_get_contents($filename);
-
-	$pieces = explode("\x0A", $data);
-
-	$keyValueArray = array();
-
-	foreach($pieces as $key => $value) {
-		$keyValueArray[] = explode("\t", $value);
+	while (!feof($file)) {
+		fgets($file);
+		$count++;
 	}
 
+	fclose($file);
+
+	return $count;
+}
+
+
+/*
+ *   Searching for a value with a key provided using the binary search algorythm
+ */
+function binarySearchByKey($filename, $key_value) {
+
+	$mem_start = memory_get_usage();
+
 	$s = 0;	// start point
-	$f = count($keyValueArray) - 1;  // end point
+	$f = getNumberOfValues($filename);  // end point
+
+	$file = new SplFileObject($filename);
 
 	while ($s <= $f) {
 
 		$half = floor(($s + $f) / 2);
 
+		$file->seek($half);	// moving to a particular line
+
+		$line = explode("\t", $file->current());  // getting a key and value separated
+
 		// key values are supposed to be in lexicographical order, so strnatcmp is used for corparison
-		$comparison = strnatcmp($keyValueArray[$half][0], $key_value);
+		$comparison = strnatcmp($line[0], $key_value);
 
 		if ($comparison > 0) {
 			$f = $half - 1;
 		} else if ($comparison < 0) {
 			$s = $half + 1;
 		} else {
-			return $keyValueArray[$half][1];
+			echo "<span style='background-color: #F5FCAC;'>Используемая память:</span> " . (memory_get_usage() - $mem_start) . " байт<br>";
+			return $line[1];
 		}
 
-		return 'undef';
 	}
-		
+	echo "<span style='background-color: #F5FCAC;'>Используемая память: </span>" . (memory_get_usage() - $mem_start) . "<br>";
+	
+	// no matches :(
+	return 'undef';		
 }
+
+$start = microtime(true);
+
 if (isset($_GET['key'])) {
 
 	$key_value = $_GET['key'];
 
 	echo "<span style='background-color: #ACF0FC;'>Полученное значение по ключу:</span>  " . binarySearchByKey('data.txt', $key_value);
-	echo "<br><span style='background-color: #FCACAC;'>Скрипт выполнен за:</span>  " . (microtime(true) - $start);
+	echo "<br><span style='background-color: #FCACAC;'>Скрипт выполнен за:</span>  " . (microtime(true) - $start) . " секунд";
 
 } else {
 
@@ -50,3 +74,4 @@ if (isset($_GET['key'])) {
 }
 
 
+?>
